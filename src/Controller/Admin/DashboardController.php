977253cc1eb668c2c9af485069d9338a4f121213
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2026-04-28 17:17:47 -0300

    Update DashboardController.php

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -54,7 +54,7 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Produtos', 'fa fa-tag', Product::class);
-        yield MenuItem::linkToCrud('Usuários', 'fa fa-user', User::class);
+        yield MenuItem::linkTo('Produtos', 'fa fa-tag', Product::class);
+        yield MenuItem::linkTo('Usuários', 'fa fa-user', User::class);
     }
 }
